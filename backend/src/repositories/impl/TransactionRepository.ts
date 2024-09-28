1bf14e65bd24e6125f733a8e8b06e229774349fd
abdul_suhaill <asuhail181996@gmail.com>
2024-09-28 06:03:15 +0530

    trigger commit

--- backend/src/repositories/impl/TransactionRepository.ts
+++ backend/src/repositories/impl/TransactionRepository.ts
@@ -19,6 +19,7 @@ export class TransactionRepository implements ITransactionRepository {
 
   private formatDateForSQLite(date: string) {
     const d = new Date(date);
+    
     return `${d.getFullYear()}-${(d.getMonth() + 1)
       .toString()
       .padStart(2, "0")}-${d.getDate().toString().padStart(2, "0")} ${d
